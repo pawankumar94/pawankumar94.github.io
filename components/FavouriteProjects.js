603f9be1c59772dd16b6c1775595a8456f3e16c3
pawankumar94 <pawan.kumar@st.ovgu.de>
2021-11-14 23:53:03 +0100

    Portfolio files added

---
+++ components/FavouriteProjects.js
@@ -0,0 +1,24 @@
+
+import React from "react";
+import Link from "next/link";
+
+export default function FavouriteProjects() {
+  return (
+    <div className="bg-[#F1F1F1] -mt-40 dark:bg-gray-900">
+      <div className="max-w-6xl mx-auto">
+      
+        
+
+        {/* Grid starts here */}
+        <div className="grid md:grid-cols-3 gap-8 lg:-mt-8 pb-40">
+          {/* Single card */}
+          
+          {/* Single card */}
+          
+          {/* Single card */}
+          
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ constants/data.js
@@ -0,0 +1,75 @@
+const userData = {
+  githubUsername: "pawankumar94",
+  name: "Pawan Kumar",
+  designation: "Machine Learning Engineer",
+  avatarUrl: "/avatar.png",
+  email: "Pawan.kumar1181994@gmail.com",
+  phone: "+49 17659507372",
+  address: "Ernst Lehmann Straße 17, 39106 Magdeburg.",
+  
+  about: {
+    title:
+      "I'm a Machine Learning and cloud infrastructure aspirant who is passionate about learning cutting edge techonolgies to solve complex business problems" ,
+    description: [
+      `I have special interest in the field of AI, I have been assosciated with the field from past 2 years by closely working on multiple projects using machine learning strategies`,
+      ` I have been working as a Researcher in the field of Machine learning from past 1 year where I have aquired special interest in the field of Dev-Ops integrated with machine 
+        learning, which motivates me to seek career as a Machine Learning Engineer.`, 
+      ` I've  used  Python, C, C++, MySql, GO, React,etc languages in past 3 for  implementing various machine learning models and des.`,
+    ],
+    currentProject: "",
+    currentProjectUrl: "",
+  },
+  experience: [
+    {
+      title: "Machine Learning Researcher",
+      company: "Leibniz University Hannover",
+      year: "2020",
+      companyLink: "https://www.braine-project.eu/project/",
+      desc: "Currently working as a researcher in a Research Institute in Hannover on a project funded by EU",
+    },
+    {
+      title: "Full Stack Web Developer",
+      company: "Magdeburg-Stendal University Of Applied Sciences",
+      year: "2020",
+      companyLink: "https://www.h2.de/home.html",
+      desc: " " ,
+    },
+    {
+      title: "Full Stack Developerr",
+      company: "Otto Von Guericke University",
+      year: "2020",
+      companyLink: "https://www.ovgu.de/",
+      desc: "",
+    },
+    {
+      title: "System Engineer",
+      company: "Infosys Ltd.",
+      year: "2015",
+      companyLink: "https://www.infosys.com/",
+      desc: "Worked for CITI-Bank client for handling and debugging issues related to Apache web servers ",
+    },
+    {
+      title: "Graduation",
+      company: "GGSIPU, Delhi.",
+      year: "2015",
+      companyLink: "http://www.ipu.ac.in/",
+      desc: "Specializaion in Computer Science and Engineering.",
+    },
+    {
+      title: "Masters",
+      company: "OVGU, Magdebugrg",
+      year: "2018",
+      companyLink: "https://www.ovgu.de/",
+      desc: "Specialization in Machine Learning, Deep Learning,Data Analysis, Dev-Ops",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1hjotiYyI63FAtTUxATluO4z712sW3zVj/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://www.instagram.com/ipawanaesthetic/",
+    linkedin: "https://www.linkedin.com/in/pawan-kumar-71819372/",
+    github: "https://github.com/pawankumar94",
+  },
+};
+
+export default userData;
